cbc3bdb88cbbf98f350d6f9e82e50fc57231139f
Rhutwij <rhutwij.tulankar@gmail.com>
2016-09-08 10:54:11 -0500

    first commit

---
+++ install.sql
@@ -0,0 +1,28 @@
+-- install:
+-- 1) blow in SQL data below
+-- 2) bizDataLayer>dataerror.log make sure the log has 777 permissions
+-- 3) svcLayer>game>gameSvc.php line 11, make sure the path to your dbInfo is correct!
+
+-- Table structure for table `checkers_games`
+-- 
+
+CREATE TABLE `checkers_games` (
+  `game_id` int(10) NOT NULL auto_increment,
+  `whoseTurn` int(1) NOT NULL default '0',
+  `player0_name` varchar(255) NOT NULL default '',
+  `player0_pieceID` text,
+  `player0_boardI` varchar(255) default NULL,
+  `player0_boardJ` varchar(255) default NULL,
+  `player1_name` varchar(255) NOT NULL default '',
+  `player1_pieceID` text,
+  `player1_boardI` varchar(255) default NULL,
+  `player1_boardJ` varchar(255) default NULL,
+  `last_updated` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`game_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+
+-- 
+-- Dumping data for table `checkers_games`
+-- 
+
+INSERT INTO `checkers_games` VALUES (38, 0, 'Dan', NULL, NULL, NULL, 'Fred', NULL, NULL, NULL, '0000-00-00 00:00:00');
